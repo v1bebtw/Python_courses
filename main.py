1aa7b5aa429ed812e540012f0d09ca320ae41f63
v1bebtw <98270719+v1bebtw@users.noreply.github.com>
2022-01-23 18:41:26 +0300

    создание обменника валют

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-a = 5
-b = 2
+usd = float(2.57)
+euro = float(2.92)
 
-c = a ** b
-print(c)
+money = int(input('Введите сумму, которую хотите обменять: '))
+currency = int(input('1 - доллары, 2 - евро: '))
+
+if currency == 1:
+    cash = round(money / usd, 2)
+    print(cash)
+elif currency == 2:
+    cash = round(money / euro, 2)
+    print(cash)
+else:
+    print('Что-то пошло не так')
+
+
