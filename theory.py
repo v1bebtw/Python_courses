0a53c98fa428f79e386ab3ed4029be72cbbe1c11
v1bebtw <98270719+v1bebtw@users.noreply.github.com>
2022-02-13 18:56:52 +0300

    Add files via upload

--- theory.py
+++ theory.py
@@ -62,7 +62,9 @@
 #     print(f'{c} нет!')
 
 """************   Условные операторы   ************"""
+
 # x = 0
+
 # y = 5
 
 # if x == 6:
@@ -165,3 +167,117 @@
 # else:
 #     print('В году 12 месяцов')
 
+"""************   Цикл while и модуль random   ************"""
+
+#import random
+#
+#
+# print(random.randint(1, 10))
+# random.randint(start, stop, step(не всегда))
+# print(random.randrange(10))
+# random.randrange(10(10 не включая))
+
+# import random
+#
+#
+# kubik1 = random.randint(1, 6)
+# kubik2 = random.randrange(6) + 1
+# total = kubik1 + kubik2
+#
+# print(f"При вашем броске выпалоk {kubik1} и {kubik2} очков. \nВ сумме {total}")
+# input("\n\nНажмите Enter, чтобы выйти ")
+
+# a = 1
+# while a != 10:
+#     print(a)
+#     a += 1
+#
+# print('Всё, конец программы, я сделал всё, что мог')
+
+# otvet = ""
+#
+# while otvet.lower() != "Покупаю":
+#     otvet = input("купи слона!\n")
+#
+#     if otvet.lower() == "покупаю":
+#         break
+#
+#     if otvet.lower() == "покупаю":
+#         print(f"Все говорят:{otvet}, а ты возьми и купи слона!")
+#
+# print("Цена на слона 1000$. Деньги вперед")
+# input("Нажмите Enter, чтобы выйти")
+
+# num = 0
+# while num < 20:
+#     num += 1
+#     print(num)
+
+# print("Вашего героя окружила огромная армия троллей. \nСмрадными зелеными трупами врагов устланы все окрестные поля. \nОдинокий герой достает меч из ножен, готовясь к последней битве в своей жизни\n")
+#
+# health = 100
+# trolls = 0
+# damage = 3
+#
+# while health > 0:
+#     trolls = trolls + 1
+#     health = health - damage
+#     print(f"Взмахнув мечом, ваш герой истребляет злобного тролля, но сам получает повреждений на {damage} очков.\n")
+#
+# print(f"Ваш герой доблестно сражался и убил {trolls} троллей.")
+# print("Но увы, он пол на поле боя.")
+# print("F герою")
+# input("\n\nНажмите Enter, чтобы выйти.")
+
+# count = 0
+#
+# while True:
+#     count += 1
+#
+#     if count > 10:
+#         break
+#
+#     if count == 5:
+#         print('Пропускаем пятерку')
+#
+#     print(count)
+
+# print("\tЭксклюзивная компьютерная сеть")
+# print("\tТолько для зарегистрированных пользователей!\n")
+#
+# security = 0
+# username = ""
+# password = ""
+#
+# while not username:
+#     username = input("Логин:")
+#
+# while not password:
+#     password = input("Пароль:")
+#
+# if username == "Egor" and password == "Egorik":
+#     print("Здравствуй, Егор!")
+#     security = 3
+#
+# elif username == "guest" and password == "guest":
+#     print("Добро пожаловать к нам в гости.")
+#     security = 1
+#
+# else:
+#     print("Войти в систему не удалось. Должно быть, Вы не зарегистрированы. \n")
+#
+# input("\n\nНажмите Enter, чтобы выйти")
+
+top = ""
+list = []
+
+while True:
+    top = input("Что вы хотите добавить в пиццу?")
+    if top == "quit":
+        print("Ваш заказ принят")
+        break
+    if top != "quit":
+        print(f"{top} добавили в пиццу")
+    list.append(top)
+
+print(list)
