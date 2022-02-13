a4be6c2e31202d30644a09bdf694b12c5756e629
v1bebtw <98270719+v1bebtw@users.noreply.github.com>
2022-02-13 15:59:19 +0300

    Add files via upload

--- theory.py
+++ theory.py
@@ -49,14 +49,119 @@
 # print(string[0:3])
 # print(string[-1])
 
-string = 'Сидел барсук в своей норе и ел картошечку пюре'
-a = '!'
-c = 'ре'
+# string = 'Сидел барсук в своей норе и ел картошечку пюре'
+# a = '!'
+# c = 'ре'
+#
+# print(len(string))
+# print(string + a)
+#
+# if c in string:
+#     print(f'{c} есть!')
+# else:
+#     print(f'{c} нет!')
 
-print(len(string))
-print(string + a)
+"""************   Условные операторы   ************"""
+# x = 0
+# y = 5
 
-if c in string:
-    print(f'{c} есть!')
-else:
-    print(f'{c} нет!')
+# if x == 6:
+#     print('Верно')
+#
+# elif x == 4:
+#     print(4)
+#
+# elif x == 3:
+#     print(3)
+#
+# else:
+#     print('Ничего не верно')
+
+# if x >= 0:
+#     print('Число больше нуля')
+#
+# print('Едем дальше')
+#
+# if x >= 0 and y >= 0:
+#     print('Оба числа больше 0')
+#
+# if x == 5 or y == 5:
+#     print('Какое-то из чисел равно 5')
+#
+# if x != 5:
+#     print('X не равно 5')
+#
+# health = int(input('Введите кол-во здоровья:'))
+#
+# if health <= 0:
+#     health = False
+#     print(health)
+#
+# else:
+#     health = True
+#     print(health)
+#
+# password = input('Введите пароль: ')
+#
+# if password == '777atom':
+#     print('Доступ к компьютеру разрешён')
+#
+# else:
+#     print('Пароль неверный. Доступ к компьютеру запрещён')
+
+# x = 10
+#
+# if x % 2 == 0:
+#     print('Число чётное')
+# else:
+#     print('Число нечётное')
+#
+# if x % 5 == 0:
+#     print('Число делиться на 5')
+# else:
+#     print('Число не делиться на 5')
+#
+# print(len(str(x))
+
+# year = int(input('Введите ваш год:'))
+#
+# if year % 4 == 0:
+#     print('Yes')
+# else:
+#     print('No')
+
+# x = int(input('Введите координату точки по оси OX:'))
+# y = int(input('Введите координату точки по оси OY:'))
+#
+# if x > 0 and y > 0:
+#     print('Точка находиться в первой четверти')
+#
+# elif x < 0 and y > 0:
+#     print('Точка находиться во второй четверти')
+#
+# elif x < 0 and y < 0:
+#     print('Точка находиться в третьей четверти')
+#
+# elif x > 0 and y < 0:
+#     print('Точка находиться в четвёртой четверти')
+#
+# else:
+#     print('Точка находиться на оси')
+#
+# month = int(input('Введите номер месяца вашего рождения:'))
+#
+# if 0 < month < 3 or month == 12:
+#     print('За окном падал белый снег')
+#
+# elif 2 < month < 6:
+#     print('Птицы пели прекрасные песни')
+#
+# elif 5 < month < 9:
+#     print('Солнце светило ярче, чем когда-либо')
+#
+# elif 8 < month < 12:
+#     print('Урожай был невероятным')
+#
+# else:
+#     print('В году 12 месяцов')
+
